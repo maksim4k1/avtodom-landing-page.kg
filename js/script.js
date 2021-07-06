15126ebbfd0d522b227a557bda571e7b4d4d331e
Maksim Bazhenov <maksim4k1@gmail.com>
2021-07-06 17:39:57 +0600

    edited animation function and animation clsses

--- js/script.js
+++ js/script.js
@@ -21,22 +21,34 @@ function toggleBurgerMenu(){
 window.addEventListener("scroll", animateDocument);
 window.addEventListener("load", animateDocument);
 
+let isFinishBrandsAnimation = false;
+let isFinishContactsAnimation = false;
 function animateDocument(){
     let brands = document.querySelector(".brands");
     let brandItems = document.querySelectorAll(".brands__item");
-    if(window.scrollY > offset(brands).top - window.screen.height + 400){
+    if(window.scrollY > (offset(brands).top - window.screen.height + 400) && isFinishBrandsAnimation === false){
         for(let i = 0; i < brandItems.length; i++){
             brandItems[i].classList.add("translateY-animation");
             brandItems[i].style.animationDelay = `0.${i}s`;
+            setTimeout(function(){
+                brandItems[i].classList.remove("translateY-animation");
+                brandItems[i].style.opacity = 1;
+            }, i * 100 + 1000);
         }
+        isFinishBrandsAnimation = true;
     }
 
     let contacts = document.querySelector(".contacts");
     let contactsList = document.querySelectorAll(".contacts__list");
-    if(window.scrollY > offset(contacts).top - window.screen.height + 400){
+    if(window.scrollY > offset(contacts).top - window.screen.height + 400 && isFinishContactsAnimation === false){
         for(let i = 0; i < contactsList.length; i++){
             contactsList[i].classList.add("translateX-animation");
             contactsList[i].style.animationDelay = `0.${i}s`;
+            setTimeout(function(){
+                contactsList[i].classList.remove("translateX-animation");
+                contactsList[i].style.opacity = 1;
+            }, i * 100 + 1000);
+            isFinishContactsAnimation = true;
         }
     }
 }
